2ba1952d57d49e93da1e1d930794255d87e31b49
mitalisahu2002 <mitalisahu1112002@gmail.com>
2024-09-27 12:37:52 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -5,4 +5,12 @@ def count_islands(map_grid):
     
     def dfs(r,c):
         if r < 0 or r>= rows or c<0 or c>= cols or map_grid[r][c] == 'W' or visited[r][c]:
-            
+            return
+        visited[r][c] = True
+        dfs(r-1,c)
+        dfs(r+1,c)
+        dfs(r,c-1)
+        dfs(r,c+1)
+    for r in range(rows):
+        for c in range(cols):
+            if map_grid[r][c] == 'L' and
