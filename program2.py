ff3927b652bf8250662381db9c35bce47de56499
mitalisahu2002 <mitalisahu1112002@gmail.com>
2024-09-27 12:22:00 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -10,4 +10,5 @@ def decoder_ring(message, decoder_key):
         else:
             if message_index < len(message) and char == message[message_index]:
                 message_index +=1
-        else
+    else:
+        
