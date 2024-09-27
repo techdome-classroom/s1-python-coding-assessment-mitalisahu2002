9fb69ad585bdcf6bf702bee5b9271fa711a6243d
mitalisahu2002 <mitalisahu1112002@gmail.com>
2024-09-27 12:31:54 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,3 +1,4 @@
 def count_islands(map_grid):
     rows, cols = len(map_grid), len(map_grid[0])
-    visited = [[False for _ in range(cols)]]
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
+    
